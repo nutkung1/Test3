8743d2900e261c8f288c86cb2f45ca8942d4260a
Suchanat-nut <78488006+nutkung1@users.noreply.github.com>
2024-02-02 15:54:43 +0700

    Update CRUD_cloud.py

--- CRUD_cloud.py
+++ CRUD_cloud.py
@@ -1,35 +1,24 @@
 import streamlit as st
 import psycopg2
-from psycopg2 import sql
-
 st.title("Hello")
-
-# Get the database URL from Streamlit secrets
 connection_string = st.secrets.db_credentials.DATABASE_URL
-
-# Connect to the database
-connection = psycopg2.connect(connection_string, sslmode='verify-ca', sslrootcert='path/to/root.crt')
+# print(st.secrets.db_credentials.db_url)
+connection = psycopg2.connect(connection_string)
 cursor = connection.cursor()
-
-# Create the 'farmer' table if it doesn't exist
-create_table_query = sql.SQL("CREATE TABLE IF NOT EXISTS farmer (id SERIAL PRIMARY KEY, name VARCHAR(20));")
-cursor.execute(create_table_query)
+create_table = "CREATE TABLE IF NOT EXISTS farmer(id int PRIMARY KEY, name varchar(20));"
+cursor.execute(create_table)
 connection.commit()
 
-# Insert a sample record into the 'farmer' table
-insert_query = sql.SQL("INSERT INTO farmer (name) VALUES (%s) RETURNING id;")
-cursor.execute(insert_query, ('John Doe',))
-connection.commit()
+# insert_query = "INSERT INTO farmer (id, name) VALUES (%s, %s);"
+# val = (1, "John Doe")
+# cursor.execute(insert_query,val)
+# connection.commit()
 
-# Retrieve and display all records from the 'farmer' table
-select_query = sql.SQL("SELECT * FROM farmer;")
+select_query = "SELECT * FROM farmer;"
 cursor.execute(select_query)
-rows = cursor.fetchall()
 
+rows = cursor.fetchall()
 for row in rows:
     st.write(row)
-
-# Close the database connection
-cursor.close()
-connection.close()
+print("Success")
 
